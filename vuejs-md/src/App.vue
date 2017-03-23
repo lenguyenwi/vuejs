1c5e40839b6b6e44943c536af21c06c070dba521
LeNGuyenWi <lenguyenwi@gmail.com>
2017-03-23 14:00:48 +0100

    vue Material Design project

---
+++ vuejs-md/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components:{
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 60px 30px 0px 30px;
+}
+</style>
---
+++ vuejs-md/src/components/Hello.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hello">
+    <!-- <hello></hello> -->
+    <md-card>
+      <md-card-header>
+        <div class="md-title">
+          Material Design With Vue.js 2 Demo
+        </div>
+        <div class="md-subhead">
+          by CodingTheSmartWay.com
+        </div>
+      </md-card-header>
+      <md-card-content>
+        <!-- <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill Table</md-button>
+        <md-button class= "md-raised md-primary" v-on:click="clearTable()">Clear Table</md-button> -->
+        <button v-on:click="fillTable()">Fill Table</button>
+        <button v-on:click="clearTable()">Clear Table</button>
+        <button v-on:click="greet('Hello World!!!')">Say Greeting</button>
+        <button v-on:click="deletefristRow()">clear 1st row</button>
+      </md-card-content>
+      <md-table>
+        <md-table-header>
+          <md-table-row>
+            <md-table-head>First name</md-table-head>
+            <md-table-head>Last name</md-table-head>
+            <md-table-head>Email</md-table-head>
+          </md-table-row>
+        </md-table-header>
+        <md-table-body>
+            <md-table-row v-for="contact in contacts">
+              <md-table-cell>{{contact.firstname}}</md-table-cell>
+              <md-table-cell>{{contact.lastname}}</md-table-cell>
+              <md-table-cell>{{contact.email}}</md-table-cell>
+            </md-table-row>
+        </md-table-body>
+      </md-table>
+    </md-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  // components:{
+  //   Hello
+  // },
+  data() {
+    return{
+      contacts: [
+        // {firstname:'lenguyenwi',lastname:'vinsmoke',email:'allbluesky@gmail.com'},
+        // {firstname:'lenguyenwi',lastname:'vinsmoke',email:'allbluesky@gmail.com'},
+        // {firstname:'lenguyenwi',lastname:'vinsmoke',email:'allbluesky@gmail.com'}
+      ]
+    }
+  },
+  methods:{
+    fillTable: function() {
+      this.contacts.push({firstname:'lenguyenwi',lastname:'vinsmoke',email:'allbluesky@gmail.com'});
+      this.contacts.push({firstname:'johhannes',lastname:'funny',email:'stupid@gmail.com'});
+      this.contacts.push({firstname:'sanji',lastname:'kun',email:'bluesea@gmail.com'})
+    },
+    clearTable: function(){
+      this.contacts.splice(0,this.contacts.length);
+    },
+    greet: function(para){
+      alert(para);
+    },
+    deletefristRow: function(){
+            this.contacts.splice(0,1);
+    }
+  },
+}
+</script>
+
+<style>
+#hello {
+  margin: 60px 30px 0px 30px;
+}
+</style>
